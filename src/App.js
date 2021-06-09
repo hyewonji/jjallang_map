83d92a836147b84135c34c70f4de19b11ab147d1
hyewon <Hyewon>
2021-06-09 14:08:19 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import styled from "styled-components";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        const container = document.getElementById("map");
+        const options = {
+            center: new kakao.maps.LatLng(33.450701, 126.570667),
+            level: 3,
+        };
+        const map = new kakao.maps.Map(container, options);
+    }, []);
+
+    return (
+        <div className="App">
+            <div id="map" style={{ width: "100vw", height: "100vh" }}></div>
+        </div>
+    );
 }
 
 export default App;
