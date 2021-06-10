f893fb23937f3b8a742ce8ad823db1279657ea99
hyewonji <wlgpdnjs8873@naver.com>
2021-06-10 13:57:26 +0900

    코인 노래방 위치 보여주기
    Kakao map의 키워드로 장소 검색하기 샘플 활용

--- src/components/kakaoMap.jsx
+++ src/components/kakaoMap.jsx
@@ -1,7 +1,6 @@
 /*global kakao*/
 
 import React, { useEffect, useState } from "react";
-// import styled from "styled-components";
 
 function KakaoMap() {
     const [coords, setCoords] = useState({
@@ -20,13 +19,43 @@ function KakaoMap() {
     }, []);
 
     useEffect(() => {
-        console.log(coords);
+        const infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
         const container = document.getElementById("map");
         const options = {
             center: new kakao.maps.LatLng(coords.latitude, coords.longitude),
-            level: 3,
+            level: 5,
         };
         const map = new kakao.maps.Map(container, options);
+        const ps = new kakao.maps.services.Places();
+        ps.keywordSearch("코인 노래방", placesSearchCB);
+
+        function placesSearchCB(data, status, pagination) {
+            if (status === kakao.maps.services.Status.OK) {
+                var bounds = new kakao.maps.LatLngBounds();
+
+                for (var i = 0; i < data.length; i++) {
+                    displayMarker(data[i]);
+                    bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+                }
+                map.setBounds(bounds);
+            }
+        }
+
+        function displayMarker(place) {
+            var marker = new kakao.maps.Marker({
+                map: map,
+                position: new kakao.maps.LatLng(place.y, place.x),
+            });
+
+            kakao.maps.event.addListener(marker, "click", function () {
+                infowindow.setContent(
+                    '<div style="padding:5px;font-size:12px;">' +
+                        place.place_name +
+                        "</div>"
+                );
+                infowindow.open(map, marker);
+            });
+        }
     }, [coords]);
 
     return <div id="map" style={{ width: "100vw", height: "100vh" }} />;
